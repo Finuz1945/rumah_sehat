82c78585ff7d219b099595329e445abd36d7739c
Firdaus Nuzula Nur Rosyid <firdausnuzula95@gmail.com>
2025-03-24 09:59:01 +0700

    connect to docker

--- pages/koneksi.php
+++ pages/koneksi.php
@@ -1,14 +1,12 @@
 <?php
 
-$server = "localhost";
+$server = "mariadb";
 $user = "root";
-$password = "";
+$password = "rootpassword";
 $nama_database = "db_rs";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
-if( !$db ){
+if (!$db) {
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
-
-?>
